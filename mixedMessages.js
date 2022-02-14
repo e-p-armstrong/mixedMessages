fb8e135b4670d69ab4937b008acf9844f8f0cfc9
Evan Armstrong <evanpeterarmstrong@gmail.com>
2022-02-14 12:59:32 -0800

    add bang

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,4 +1,4 @@
-//This program shall insult the user.
+//This program shall insult the user. It's the first thing that came to mind. Is this why I have no friends?
 
 const userRelatedNounArray = ["Mother","Father","Dog","Cat","Daughter","Son","Second Cousin Twice Removed","Brother","Sister"];
 const insultNounArray = [["idiot","an"],["moron","a"],["scoundrel","a"],["knave","a"],["cur","a"],["slut","a"],["whore","a"],["bastard","a"],["vagabond","a"],["lowlife","a"],["pig","a"],["psycho","a"],] //The second element of each item array specifies whether to use 'an' or 'a' -- grammatical correctness is important!
@@ -9,7 +9,7 @@ function insult(){
     const secondNounArray = insultNounArray[Math.floor((Math.random() * userRelatedNounArray.length))]
     const secondNoun = secondNounArray[0]
     const particle = secondNounArray[1]
-    return("\nYour " + firstNoun + " is " + particle + " " + secondNoun + "\n")
+    return("\nYour " + firstNoun + " is " + particle + " " + secondNoun + "!\n")
 }
 
 console.log(insult())
